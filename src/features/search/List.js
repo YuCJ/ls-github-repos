7cf26c6dc0d259abd4da99d9a2b1f54b4d3ab209
yucj <nakts0123@gmail.com>
2020-11-14 16:25:46 +0800

    refactor: make type converting more explicit

--- src/features/search/List.js
+++ src/features/search/List.js
@@ -4,8 +4,10 @@ import React from 'react';
 
 function List() {
   const repoIdsByPage = useSelector(state => state.searchRepos.repoIdsByPage);
-  const pages = Object.keys(repoIdsByPage).sort((a, b) => a - b);
-  return pages.map((page, i) => <Page key={i} page={page} />);
+  const pages = Object.keys(repoIdsByPage).sort(
+    (a, b) => parseInt(a, 10) - parseInt(b, 10)
+  );
+  return pages.map(page => <Page key={page} page={page} />);
 }
 
 export default List;
